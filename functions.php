3a4c2e5d6f47fd3f2f86f2aeaea8e1d2cb30cea8
Phil Gyford <phil@gyford.com>
2014-08-10 10:21:05 +0100

    Tweak the display of entry-meta info

--- functions.php
+++ functions.php
@@ -206,4 +206,44 @@ function whitstillman_set_specific_header() {
 // No idea if this is the ideal place to run this, but it seems to work.
 add_action('get_header', 'whitstillman_set_specific_header');
 
+
+
+/**
+ * A copy of the parent's function, removing the category and tag elements.
+ */
+function twentythirteen_entry_meta() {
+	if ( is_sticky() && is_home() && ! is_paged() )
+		echo '<span class="featured-post">' . __( 'Sticky', 'twentythirteen' ) . '</span>';
+
+	if ( ! has_post_format( 'link' ) && 'post' == get_post_type() )
+		twentythirteen_entry_date();
+
+	// Post author
+	if ( 'post' == get_post_type() ) {
+		printf( '<span class="author vcard">By: <a class="url fn n" href="%1$s" title="%2$s" rel="author">%3$s</a></span>',
+			esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+			esc_attr( sprintf( __( 'View all posts by %s', 'twentythirteen' ), get_the_author() ) ),
+			get_the_author()
+		);
+	}
+}
+
+/**
+ * The category and tag elements from twentythirteen_entry_meta(), which we 
+ * want to output separately.
+ * Used in content.php.
+ */
+function whitstillman_entry_meta_extra() {
+	// Translators: used between list items, there is a space after the comma.
+	//$categories_list = get_the_category_list( __( ', ', 'twentythirteen' ) );
+	//if ( $categories_list ) {
+		//echo '<span class="categories-links">Category: ' . $categories_list . '</span>';
+	//}
+
+	// Translators: used between list items, there is a space after the comma.
+	$tag_list = get_the_tag_list( '', __( ', ', 'twentythirteen' ) );
+	if ( $tag_list ) {
+		echo '<span class="tags-links">Tagged: ' . $tag_list . '</span>';
+	}
+}
 ?>
