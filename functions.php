7812f6a7c28870d87b2579fee9e060c2cdfb4a36
Phil Gyford <phil@gyford.com>
2015-08-26 09:27:01 +0100

    Hopefully fix issue with parent styles not being included

--- functions.php
+++ functions.php
@@ -9,6 +9,31 @@
 
 /* This is loaded BEFORE the parent's functions.php. */
 
+
+/**
+ * Include parent theme scripts and styles.
+ */
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+
+/**
+ * Queue parent theme scripts and styles.
+ */
+function theme_enqueue_styles() {
+
+    $parent_style = 'parent-style';
+
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style )
+    );
+}
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+
 /**
  * We don't use theme thumbnails.
  */
