e71f19dec430fc609aae0fcf579b0ec363a89e60
Phil Gyford <phil@gyford.com>
2015-08-26 09:30:01 +0100

    Fix stupid error with enqueuing parent styles

--- functions.php
+++ functions.php
@@ -11,14 +11,6 @@
 
 
 /**
- * Include parent theme scripts and styles.
- */
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-function theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-}
-
-/**
  * Queue parent theme scripts and styles.
  */
 function theme_enqueue_styles() {
